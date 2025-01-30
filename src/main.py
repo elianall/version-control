1bab73240b0bc17a71dca98dc08f61db556a1fd9
elianal1 <el@class-docker>
2025-01-30 08:22:03 +0000

    First commit.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-From datetime import datetime
 
-# Get the current date and time in the format YYYY-MM-DD HH:MM:ss
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+from datetime import datetime
 
-# Write to version.md
-with open('version.md', 'w') as file:
-    file.write(f"Version created on: {current_time}\n")
+now = datetime.now()
+formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+print(formatted_date_time)
