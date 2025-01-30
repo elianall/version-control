5c5d82212c54c0dd9c6c85691d0cf9565cce938d
elianal1 <el@class-docker>
2025-01-30 08:29:17 +0000

    First commit.

--- src/main.py
+++ src/main.py
@@ -4,3 +4,6 @@ from datetime import datetime
 now = datetime.now()
 formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 print(formatted_date_time)
+
+with open("version.md", "w") as f:
+    f.write(formatted_date_time)
