323158f6d551fd70ad01cc9e56cb907c0719b723
elianal1 <el@class-docker>
2025-01-30 08:03:18 +0000

    First commit.

---
+++ src/main.py
@@ -0,0 +1,8 @@
+rom datetime import datetime
+
+# Get the current date and time in the format YYYY-MM-DD HH:MM:ss
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Write to version.md
+with open('version.md', 'w') as file:
+    file.write(f"Version created on: {current_time}\n")
