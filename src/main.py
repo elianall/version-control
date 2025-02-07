f10227778c134b122f2e4ee12a6ce3fa0035134e
elianal1 <el@class-docker>
2025-02-07 16:01:58 +0000

    First commit.

--- src/main.py
+++ src/main.py
@@ -4,3 +4,11 @@ now = datetime.now()
 formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 print(formatted_date_time)
 
+docs_folder = os.path.join(os.path.dirname(__file__), '..', '..', 'docs')
+version_file_path = os.path.join(docs_folder, 'version.md')
+
+with open(version_file_path, 'w') as file:
+    file.write(f'# Version Control\n\nCurrent Date and Time: {current_time}')
+
+print(f"Version written to {version_file_path}")
+
