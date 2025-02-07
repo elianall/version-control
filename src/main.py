e19164df4aeb178557bc1447fb4a4b7200a8c221
elianal1 <el@class-docker>
2025-02-07 20:14:50 +0000

    First commit.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+import os
 
 now = datetime.now()
 date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 print(date_time)
 
-docs_folder = os.path.join(os.path.dirname(__file__), '..', '..', 'docs')
+repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+docs_folder = os.path.join(repo_root, 'docs')
 version_file_path = os.path.join(docs_folder, 'version.md')
 
 with open(version_file_path, 'w') as file:
